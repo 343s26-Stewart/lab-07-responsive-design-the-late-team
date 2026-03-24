6b325e3a8ab8ff606d6a4daf96a11dafe1219d7f
Patrick Jabalee <jabalepx@dukes.jmu.edu>
2026-03-24 00:15:47 -0400

    fixed dark mode

--- scripts/dark.js
+++ scripts/dark.js
@@ -1,24 +1,21 @@
 document.addEventListener("DOMContentLoaded", function () {
-
     const darkBtn = document.getElementById("dark");
     const lightBtn = document.getElementById("light");
     const defaultBtn = document.getElementById("default");
 
-    const darkSheet = document.getElementById("darkmode");
-
     darkBtn.addEventListener("click", function(e) {
         e.preventDefault();
-        darkSheet.disabled = false;
+        document.body.classList.remove("force-light");
+        document.body.classList.add("force-dark");
     });
-
     lightBtn.addEventListener("click", function(e) {
         e.preventDefault();
-        darkSheet.disabled = true;
+        document.body.classList.remove("force-dark");
+        document.body.classList.add("force-light");
     });
-
     defaultBtn.addEventListener("click", function(e) {
         e.preventDefault();
-        darkSheet.disabled = true;
+        document.body.classList.remove("force-dark");
+        document.body.classList.remove("force-light");
     });
-
 });
